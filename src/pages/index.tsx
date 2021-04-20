4188a4650f779a40f98674c988ebdda42b92ae84
kaique caires <kaique.caires@yahoo.com>
2021-04-20 09:38:29 -0300

    feat: add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div className={styles.title}>Hello world</div>
   )
 }
