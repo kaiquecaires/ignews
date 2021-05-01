d39ca8a9dc02fda1de36065a69238920793288ac
kaique caires <kaique.caires@yahoo.com>
2021-05-01 19:42:59 -0300

    feat: create prismic client

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Prismic from '@prismicio/client'
+import { getPrismicCLient } from '../../services/prismic'
 import styles from './styles.module.scss'
 
 export default function Posts() {
@@ -31,4 +34,21 @@ export default function Posts() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicCLient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post'),
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100
+  })
+
+  console.log(response)
+
+  return {
+    props: {}
+  }
 }
