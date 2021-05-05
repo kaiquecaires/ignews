149fa51070a1819f880adf49444695f1c5c54879
kaique caires <kaique.caires@yahoo.com>
2021-05-05 18:31:21 -0300

    feat: verify subscription to view post

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 import { query as q } from 'faunadb'
 import { fauna } from '../../../services/faunadb'
+import { session } from 'next-auth/client'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,6 +15,43 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
+
     async signIn(user, acccount, profile) {
       const { email } = user
 
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,6 +38,16 @@ export default function Post({ post }: PostProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req })
+  
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   const { slug } = params
   const prismic = getPrismicCLient(req)
   const response = await prismic.getByUID('post', String(slug), {})
